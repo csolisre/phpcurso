a1ea544d09dfeac4447a529c1d47d66b741d0560
Carlos Solis <csolisre@gmail.com>
2018-02-01 16:00:29 +0100

    01 feb vdab

--- js/myjs.js
+++ js/myjs.js
@@ -3,11 +3,33 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-let alleBolletjes = document.getElementsByTagName('li');
-let aantalBolletjes = alleBolletjes.length;
-let tweedeItem = alleBolletjes[1];
-tweedeItem.removeChild(tweedeItem.childNodes[0]);
-let tekst = document.createTextNode('C#');
-tweedeItem.appendChild(tekst);
 
- 
+//Modificar el contenido del html
+
+//let titel = document.getElementById('kop');
+//titel.innerHTML="primer Javascript";
+//
+////Eliminar e insertar elementos dentro de una lista
+//
+//let allList = document.getElementsByTagName('li');//usamos ElementsByTagName porque el ul elemento no tiene "id"
+//let  listCount = allList.length; //lenght nos dice el numero de elementos
+//let newItem = allList[1];// los elementos inician con [0] pero cambiamos el uno
+//newItem.removeChild (newItem.childNodes[0]);//Remueve el elemento de la lista para dar entrada al nuevo
+//let text = document.createTextNode('#C');// Crea el nuevo elemento
+//newItem.appendChild(text);// Inserta el elemento
+//
+//
+////Insertar un elemento al final de la lista
+//
+//let list = document.getElementsByTagName('ul')[0];//selectionamos la lista en caso de haber mas de una lista en el documento
+//let newItem1 = document.createElement('li');//creamos el nuevo elemento
+//let text1 = document.createTextNode('Perl');//creamos el texto
+//newItem1.appendChild(text1);//anadimos el texto al elementi li
+//list.appendChild(newItem1);// //insertamos el elemento
+//
+//let som = "blabla" * 2; //geeft NaN
+//isNaN(som); //geeft true 
+
+   function hola (){
+                        alert("Hi");
+                }
